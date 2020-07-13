ae368a1a85a982e4bc7faa45e3886aa1c8669cd5
Stefan Lienhard <stefan.lienhard@virtamed.com>
2020-07-13 15:23:39 +0200

    instance creation

--- src/niagara.cpp
+++ src/niagara.cpp
@@ -1,14 +1,26 @@
 #include <assert.h>
-#include <stdio.h>
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.h>
 
+#define VK_CHECK(call) \
+	do { \
+		VkResult rc = call; \
+		assert(rc == VK_SUCCESS); \
+	} while(0)
+
 int main()
 {
-	printf("Hello Vulkan!\n");
-
 	int rc = glfwInit();
 	assert(rc == 1);
+
+	// SHORTCUT: Check if version is available via vkEnumerateInstanceVersion()
+	VkApplicationInfo app_info = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+	app_info.apiVersion = VK_VERSION_1_2;
+
+	VkInstance instance = 0;
+	VkInstanceCreateInfo create_info = { VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+	create_info.pApplicationInfo = &app_info;
+	VK_CHECK(vkCreateInstance(&create_info, nullptr, &instance));
 
 	GLFWwindow* window;
 	window = glfwCreateWindow(1024, 768, "Hello Vulkan", nullptr, nullptr);
