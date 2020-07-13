6b1968b9f645aa8daf8a425c49770b2286c1584e
Stefan Lienhard <stefan.lienhard@virtamed.com>
2020-07-13 11:59:49 +0200

    glfw window; abuse assert for error checking

--- src/niagara.cpp
+++ src/niagara.cpp
@@ -1,6 +1,23 @@
+#include <assert.h>
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
 	printf("Hello Vulkan!\n");
+
+	int rc = glfwInit();
+	assert(rc == 1);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1024, 768, "Hello Vulkan", nullptr, nullptr);
+	assert(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	return 0;
 }
