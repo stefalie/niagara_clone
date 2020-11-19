979c3152762a6124de2fd8d418babe6710a15f0c
Stefan Lienhard <stefan.lienhard@virtamed.com>
2020-11-19 23:39:12 +0100

    more unpacking before halfs

--- src/shaders/meshlet.mesh.glsl
+++ src/shaders/meshlet.mesh.glsl
@@ -1,9 +1,13 @@
 #version 450
 
+#define USE_UNPACK 1
+
+#if USE_UNPACK
+#extension GL_NV_mesh_shader : require
 //#extension GL_EXT_shader_16bit_storage : require
 #extension GL_EXT_shader_8bit_storage : require
-#extension GL_NV_mesh_shader : require
 #extension GL_EXT_shader_explicit_arithmetic_types_int8: require
+#endif
 
 layout(local_size_x = 32, local_size_y = 1, local_size_z = 1) in;
 layout(triangles, max_vertices = 64, max_primitives = 42) out;
@@ -11,7 +15,11 @@ layout(triangles, max_vertices = 64, max_primitives = 42) out;
 struct Vertex
 {
 	float vx, vy, vz;
+#if USE_UNPACK
+	uint n_packed;
+#else
 	uint8_t nx, ny, nz, nw;
+#endif
 	float tu, tv;
 };
 
@@ -50,7 +58,11 @@ void main()
 		const uint vi = meshlets[mi].vertices[i];
 
 		const vec3 position = vec3(vertices[vi].vx, vertices[vi].vy, vertices[vi].vz);
+#if USE_UNPACK
+		const vec3 normal = unpackSnorm4x8(0x80808080 ^ vertices[vi].n_packed).xyz;
+#else
 		const vec3 normal = vec3(vertices[vi].nx, vertices[vi].ny, vertices[vi].nz) / 127.0 - vec3(1.0);
+#endif
 		const vec2 uv = vec2(vertices[vi].tu, vertices[vi].tv);
 
 		gl_MeshVerticesNV[i].gl_Position = vec4(position * vec3(1, 1, 0.5) + vec3(0, 0, 0.5), 1.0);
